eb4bad351502c1f0911d135ac2bd8981dcb5758f
John.Lockwood <johnwlockwood@gmail.com>
2017-08-11 16:52:25 -0700

    Add react-router
    Add example of react-router

--- greens/src/App.js
+++ greens/src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 import carrot from './cyberscooty-carrot-300px.png'
 import Widget from './Widget'
+
 
 class App extends Component {
   constructor(props) {
@@ -30,21 +36,75 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React. {this.state.body}</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p><button onClick={(e) => this.fetchHello()}>fetch data from python</button></p>
+      <Router>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React. {this.state.body}</h2>
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p><button onClick={(e) => this.fetchHello()}>fetch data from python</button></p>
 
-        <Widget {...this.state} fetchRandomNumber={this.fetchRandomNumber.bind(this)} />
-        
-      </div>
+          <Widget {...this.state} fetchRandomNumber={this.fetchRandomNumber.bind(this)} />
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/topics">Topics</Link></li>
+          </ul>
+
+          <hr/>
+
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" render={() => <Widget {...this.state} fetchRandomNumber={this.fetchRandomNumber.bind(this)} />} />
+          <Route path="/topics" component={Topics}/>
+        </div>
+      </Router>
     );
   }
 }
 
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+    <img src={carrot} alt="carrot" />
+  </div>
+)
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a topic.</h3>
+    )}/>
+  </div>
+)
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
+
 export default App;
+
--- greens/src/Widget.js
+++ greens/src/Widget.js
@@ -5,7 +5,7 @@ class Widget extends Component
     render(){
         return (
             <div>
-            <p><button onClick={(e) => this.props.fetchRandomNumber()}>fetch random</button></p>
+            <p><button onClick={(e) => this.props.fetchRandomNumber()}>fetch random string</button></p>
             <p><b>{this.props.randomNumber} a little teacup</b></p>
             </div>
         )
